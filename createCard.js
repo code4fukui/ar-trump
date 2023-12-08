ec027b4a9556d547a68d8c042fbe7eb48a7d223c
Taisuke Fukuno <fukuno@jig.jp>
2023-12-08 10:59:00 +0900

    tab → spc

--- createCard.js
+++ createCard.js
@@ -3,29 +3,29 @@ import { createImage } from "./createImage.js";
 import { createMaterialDoubleSide } from "./createMaterialDoubleSide.js";
 
 export const createCard = (suit, num) => {
-	// create texture
+  // create texture
   const g = createImage(100, 156);
-	g.font = `bold 50px sans-serif`;
-	g.fillStyle = "white";
-	g.fillRect(0, 0, 100, 156);
-	g.fillStyle = suit % 2 == 0 ? "black" : "red";
-	g.fillTextCenter("♠♥☘◆"[suit], 50, 60);
-	g.fillTextCenter(num, 50, 130);
-	const texture = g.toTexture();
-	
-	const loader = new THREE.TextureLoader();
-	const texback = loader.load("cardback.jpg");
+  g.font = `bold 50px sans-serif`;
+  g.fillStyle = "white";
+  g.fillRect(0, 0, 100, 156);
+  g.fillStyle = suit % 2 == 0 ? "black" : "red";
+  g.fillTextCenter("♠♥☘◆"[suit], 50, 60);
+  g.fillTextCenter(num, 50, 130);
+  const texture = g.toTexture();
+  
+  const loader = new THREE.TextureLoader();
+  const texback = loader.load("cardback.jpg");
 
-	// create plane geometry
+  // create plane geometry
   const sw = 4;
-	const w = 0.057;
-	const h = 0.089;
-	const planeGeometry = new THREE.PlaneGeometry(w, h, sw, sw);
-	const material = createMaterialDoubleSide(texture, texback);
+  const w = 0.057;
+  const h = 0.089;
+  const planeGeometry = new THREE.PlaneGeometry(w, h, sw, sw);
+  const material = createMaterialDoubleSide(texture, texback);
 
-	const mesh = new THREE.Mesh(planeGeometry, material);
-	mesh.suit = suit;
-	mesh.num = num;
-	mesh.animate = t => {};
-	return mesh;
+  const mesh = new THREE.Mesh(planeGeometry, material);
+  mesh.suit = suit;
+  mesh.num = num;
+  mesh.animate = t => {};
+  return mesh;
 };
--- createImage.js
+++ createImage.js
@@ -4,12 +4,12 @@ export const createImage = (w, h) => {
   const canvas = document.createElement("canvas");
   canvas.width = w;
   canvas.height = h;
-	const g = canvas.getContext("2d");
-	g.toTexture = () => new THREE.CanvasTexture(canvas);
-	g.fillTextCenter = (s, x, y) => {
-		const m = g.measureText(s);
-		const sw = m.width;
-		g.fillText(s, x - sw / 2, y);
-	};
-	return g;
+  const g = canvas.getContext("2d");
+  g.toTexture = () => new THREE.CanvasTexture(canvas);
+  g.fillTextCenter = (s, x, y) => {
+    const m = g.measureText(s);
+    const sw = m.width;
+    g.fillText(s, x - sw / 2, y);
+  };
+  return g;
 };
